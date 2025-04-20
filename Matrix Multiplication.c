5013f1576700cc8a0243a9986fe5aca061dec424
Mustak  A Sagor <mustak.centre@gmail.com>
2025-04-21 00:37:55 +0530

    Add files via upload

--- Matrix Multiplication.c
+++ Matrix Multiplication.c
@@ -1,65 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int m, n, p, q;
-    int i, j, k;
+    int a[2][2] = {{1, 2}, {3, 4}};
+    int b[2][2] = {{5, 6}, {7, 8}};
+    int result[2][2];
 
-    // Input dimensions of Matrix A
-    printf("Enter rows and columns of Matrix A: ");
-    scanf("%d%d", &m, &n);
-
-    // Input dimensions of Matrix B
-    printf("Enter rows and columns of Matrix B: ");
-    scanf("%d%d", &p, &q);
-
-    // Check if multiplication is possible
-    if (n != p) {
-        printf("Matrix multiplication not possible. Columns of A must match rows of B.\n");
-        return 1;
-    }
-
-    int A[m][n], B[p][q], result[m][q];
-
-    // Input Matrix A
-    printf("Enter elements of Matrix A:\n");
-    for (i = 0; i < m; i++) {
-        for (j = 0; j < n; j++) {
-            printf("A[%d][%d]: ", i, j);
-            scanf("%d", &A[i][j]);
-        }
-    }
-
-    // Input Matrix B
-    printf("Enter elements of Matrix B:\n");
-    for (i = 0; i < p; i++) {
-        for (j = 0; j < q; j++) {
-            printf("B[%d][%d]: ", i, j);
-            scanf("%d", &B[i][j]);
-        }
-    }
-
-    // Initialize result matrix to 0
-    for (i = 0; i < m; i++) {
-        for (j = 0; j < q; j++) {
+    for (int i = 0; i < 2; i++)
+        for (int j = 0; j < 2; j++) {
             result[i][j] = 0;
+            for (int k = 0; k < 2; k++)
+                result[i][j] += a[i][k] * b[k][j];
         }
-    }
 
-    // Matrix multiplication
-    for (i = 0; i < m; i++) {
-        for (j = 0; j < q; j++) {
-            for (k = 0; k < n; k++) {
-                result[i][j] += A[i][k] * B[k][j];
-            }
-        }
-    }
-
-    // Display result
-    printf("Resultant Matrix (A x B):\n");
-    for (i = 0; i < m; i++) {
-        for (j = 0; j < q; j++) {
-            printf("%d\t", result[i][j]);
-        }
+    printf("Multiplication of Matrix:\n");
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++)
+            printf("%d ", result[i][j]);
         printf("\n");
     }
 
